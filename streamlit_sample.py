288118e314e278ab17aa46a3109a1be8da5a3a25
ichhi <94831526+ichhi@users.noreply.github.com>
2021-11-29 13:00:31 +0900

    4

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -50,30 +50,31 @@ if __name__ == '__main__':
     main() 
     
 @st.cache(allow_output_mutation=True)
-def cache_lst1():
-    lst1 = []
-    return lst1
-lst1 = cache_lst1()
+def cache_lst():
+    lst = []
+    return lst
 
+lst = cache_lst()
 input = st.text_input('課題1の内容')
 
-
+if input:
+    lst.append(input)
 if st.checkbox('delete1'):
-    delete1 = st.selectbox('課題1の削除する要素を選択して下さい', options=lst1)
+    delete = st.selectbox('課題1の削除する要素を選択して下さい', options=lst)
     if st.button('Delete1'):
-        lst1.remove(delete1)
-        st.success(f'Delete1 : {delete1}')
+        lst.remove(delete)
+        st.success(f'Delete1 : {delete}')
 
 if st.checkbox('change1'):
-    change1_from = st.selectbox('課題1の変更する要素を選択して下さい', options=lst1)
-    change1_index = lst1.index(change1_from)
-    change1_to = st.text_input('どのように課題1を変更しますか')
+    change_from = st.selectbox('課題1の変更する要素を選択して下さい', options=lst)
+    change_index = lst.index(change_from)
+    change_to = st.text_input('どのように課題1を変更しますか')
     if st.button('Change1'):
-        lst1.remove(change1_from)
-        lst1.insert(change1_index, change1_to)
-        st.success(f'Change1 {change1_from} to {change1_to}')
+        lst.remove(change_from)
+        lst.insert(change_index, change_to)
+        st.success(f'Change1 {change_from} to {change_to}')
         
-st.table(lst1)
+st.table(lst)
 
 
 
@@ -105,36 +106,36 @@ if __name__ == '__main__':
     main() 
     
 @st.cache(allow_output_mutation=True)
-def cache_lst2():
-    lst2 = []
-    return lst2
-lst2 = cache_lst2()
+def cache_lst():
+    lst = []
+    return lst
 
+lst = cache_lst()
 input = st.text_input('課題2の内容')
 
-
+if input:
+    lst.append(input)
 if st.checkbox('delete2'):
-    delete2 = st.selectbox('課題2で削除する要素を選択して下さい', options=lst2)
+    delete = st.selectbox('課題2の削除する要素を選択して下さい', options=lst)
     if st.button('Delete2'):
-        lst2.remove(delete2)
-        st.success(f'Delete2 : {delete2}')
+        lst.remove(delete)
+        st.success(f'Delete2 : {delete}')
 
 if st.checkbox('change2'):
-    change2_from = st.selectbox('変更する課題2の要素を選択して下さい', options=lst2)
-    change2_index = lst2.index(change2_from)
-    change2_to = st.text_input('課題2をどのように変更しますか')
+    change_from = st.selectbox('課題2の変更する要素を選択して下さい', options=lst)
+    change_index = lst.index(change_from)
+    change_to = st.text_input('どのように課題2を変更しますか')
     if st.button('Change2'):
-        lst2.remove(change2_from)
-        lst2.insert(change2_index, change2_to)
-        st.success(f' {change2_from} to {change2_to}')
+        lst.remove(change_from)
+        lst.insert(change_index, change_to)
+        st.success(f'Change2 {change_from} to {change_to}')
         
-st.table(lst2)
+st.table(lst)
 
 
 
 progress2 = st.slider('課題2の進捗は？',0, 100, 50)
 '課題2進捗：',progress2
-
 
 left_column,right_column = st.columns(2)  
 
@@ -158,34 +159,42 @@ def main():
            
 if __name__ == '__main__':
     main() 
-    
-@st.cache(allow_output_mutation=True)
-def cache_lst3():
-    lst3 = []
-    return lst3
-lst3 = cache_lst3()
 
+@st.cache(allow_output_mutation=True)
+def cache_lst():
+    lst = []
+    return lst
+
+lst = cache_lst()
 input = st.text_input('課題3の内容')
 
-
+if input:
+    lst.append(input)
 if st.checkbox('delete3'):
-    delete3 = st.selectbox('削除する課題3の要素を選択して下さい', options=lst3)
+    delete = st.selectbox('課題3の削除する要素を選択して下さい', options=lst)
     if st.button('Delete3'):
-        lst3.remove(delete3)
-        st.success(f'Delete3 : {delete3}')
+        lst.remove(delete)
+        st.success(f'Delete3 : {delete}')
 
 if st.checkbox('change3'):
-    change3_from = st.selectbox('変更する課題3の要素を選択して下さい', options=lst3)
-    change3_index = lst3.index(change3_from)
-    change3_to = st.text_input('課題3をどのように変更しますか')
-    if st.button('Change'):
-        lst3.remove(change3_from)
-        lst3.insert(change3_index, change3_to)
-        st.success(f'Change {change3_from} to {change3_to}')
+    change_from = st.selectbox('課題3の変更する要素を選択して下さい', options=lst)
+    change_index = lst.index(change_from)
+    change_to = st.text_input('どのように課題3を変更しますか')
+    if st.button('Change3'):
+        lst.remove(change_from)
+        lst.insert(change_index, change_to)
+        st.success(f'Change3 {change_from} to {change_to}')
         
-st.table(lst3)
+st.table(lst)
 
 
 
-progress = st.slider('課題3の進捗は？',0, 100, 50)
-'課題3進捗：',progress
+progress3 = st.slider('課題3の進捗は？',0, 100, 50)
+'課題3進捗：',progress3
+
+
+
+
+
+
+
